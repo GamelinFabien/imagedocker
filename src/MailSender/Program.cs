92a8343ca62d1bfaff139eea84aeecf2cccd46a7
Fabien <gamelinfabien@gmail.com>
2017-04-24 19:34:45 +0200

    ajout fichier dotnet

---
+++ src/MailSender/Program.cs
@@ -0,0 +1,19 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MailSender
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("je te parle depuis docker");
+            Console.ReadLine();
+        }
+    }
+}
